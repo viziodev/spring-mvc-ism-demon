37802c3b4887d942791950935f69a2f304b4c684
douvewane <douvewane85@gmail.com>
2024-02-17 16:39:27 +0000

    Formulaire Ajout Client

--- src/main/java/com/ism/commande/web/controllers/impl/ClientControllerImpl.java
+++ src/main/java/com/ism/commande/web/controllers/impl/ClientControllerImpl.java
@@ -3,13 +3,18 @@ package com.ism.commande.web.controllers.impl;
 import com.ism.commande.data.entities.Client;
 import com.ism.commande.data.repositories.ClientRepository;
 import com.ism.commande.web.controllers.ClientController;
+import com.ism.commande.web.dtos.ClientDtoForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
@@ -29,11 +34,36 @@ public class ClientControllerImpl  implements ClientController {
                 model.addAttribute("pages",new int[pageClients.getTotalPages()]);
                 model.addAttribute("currentPage",page);
                 model.addAttribute("keyword",keyword);
-                return "client";
+                return "client/client";
     }
     @Override
     public String index(){
         return "redirect:/api/v1/commande/liste-clients";
+    }
+
+    @GetMapping("admin/form-client")
+    public String loadForClient(Model model){
+        ClientDtoForm client=ClientDtoForm.builder().build();
+        model.addAttribute("client",client);
+        return "client/form.client";
+    }
+
+    @PostMapping("admin/save-client")
+    public String saveClient(
+            @Valid ClientDtoForm clientDtoForm,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes){
+        if(bindingResult.hasErrors()){
+            redirectAttributes.addFlashAttribute("mode","error");
+            redirectAttributes.addFlashAttribute("message","Erreur de Saisie");
+        }else{
+            redirectAttributes.addFlashAttribute("mode","succes");
+            redirectAttributes.addFlashAttribute("message","Client Enregistre avec Success");
+            Client client=clientDtoForm.toEntity();
+            clientRepository.save(client);
+
+        }
+        return "redirect:/admin/form-client";
     }
 
 }
---
+++ src/main/java/com/ism/commande/web/dtos/ClientDtoForm.java
@@ -0,0 +1,49 @@
+package com.ism.commande.web.dtos;
+
+
+import com.ism.commande.data.entities.Adresse;
+import com.ism.commande.data.entities.Client;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ClientDtoForm {
+
+    protected Long id;
+    @NotEmpty(message = "Le Nom est Obligatoire")
+    protected String nomComplet;
+    @NotEmpty(message = "Le Telephone est Obligatoire")
+    private String telephone;
+    private String ville;
+    private String quartier;
+    private String numVilla;
+
+    public Client toEntity(){
+        return new Client(
+                nomComplet,
+                telephone,
+                null,
+                new Adresse(
+                        ville,
+                        quartier,
+                        numVilla
+                )
+        );
+    }
+
+    public ClientDtoForm toDto(Client client){
+        if(client==null)  return null;
+        return ClientDtoForm.builder()
+                .id(client.getId())
+                .nomComplet(client.getNomComplet())
+                .telephone(client.getTelephone())
+                .numVilla(client.getAdresse().getNumVilla())
+                .ville(client.getAdresse().getVille())
+                .quartier(client.getAdresse().getQuartier())
+                .build();
+    }
+}
